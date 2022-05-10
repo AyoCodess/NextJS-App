35be5a493e41844426d6e90ffe417826013003d6
AyoCodess <ayo.daniel.adesanya@gmail.com>
2022-05-10 16:10:58 +0200

    shollow URL router leanring

---
+++ pages/events.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+
+function Events({ eventList }) {
+  const [events, setEvents] = useState(eventList);
+  const router = useRouter();
+
+  const fetchSportsEvents = async () => {
+    const response = await fetch(
+      `http://localhost:4000/events?category=sports`
+    );
+    const data = await response.json();
+    setEvents(data);
+    router.push('/events?category=sports', undefined, { shallow: true });
+  };
+
+  return (
+    <>
+      <button onClick={fetchSportsEvents}>sports events</button>
+      <h1>List of Events</h1>
+      {events.map((event) => (
+        <div key={event.id}>
+          <h2>
+            {event.id} {event.title} {event.data} | {event.category}
+          </h2>
+          <p>{event.description}</p>
+          <hr />
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default Events;
+
+export const getServerSideProps = async (context) => {
+  const { query } = context;
+  const { category } = query;
+  const queryString = category ? 'category=sports' : '';
+
+  const response = await fetch(`http://localhost:4000/events?${queryString}`);
+  const data = await response.json();
+
+  return {
+    props: {
+      eventList: data,
+    },
+  };
+};

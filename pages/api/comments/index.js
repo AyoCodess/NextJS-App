4f2ad0dbf3dbca7b7096feb0bc32c326bdc3351a
AyoCodess <ayo.daniel.adesanya@gmail.com>
2022-05-12 14:50:53 +0200

    implemented api routes and functionality for GET and POST

--- pages/api/comments/index.js
+++ pages/api/comments/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
 import { comments } from '../../../data/comments';
 
 function handler(req, res) {
-  res.status(200).json(comments);
+  if (req.method === 'GET') {
+    res.status(200).json(comments);
+  }
+
+  if (req.method === 'POST') {
+    const comment = req.body.comment; //-comment refers to the property we set in the front-end
+
+    const newComment = { id: Date.now().valueOf(), text: comment };
+
+    comments.push(newComment);
+
+    res.status(201).json(newComment);
+  }
 }
 
 export default handler;
--- pages/comments/index.js
+++ pages/comments/index.js
@@ -2,6 +2,19 @@ import React, { useState } from 'react';
 
 function CommentsPage() {
   const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState('');
+
+  const submitComment = async () => {
+    const response = await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({ comment }),
+      headers: { 'Content-Type': 'application/Json' },
+    });
+
+    const data = await response.json();
+    console.log(data);
+  };
+
   const fetchComments = async () => {
     const response = await fetch('/api/comments');
     const data = await response.json();
@@ -10,6 +23,12 @@ function CommentsPage() {
 
   return (
     <>
+      <input
+        type='text'
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+      />
+      <button onClick={submitComment}>submit comments</button>
       <button onClick={fetchComments}>Load comments</button>
       {comments.map((comment) => (
         <div key={comment.id}>
